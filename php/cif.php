37795deb39b12498cda23b6f9d6a6701fee70fba
Herman Bergwerf <hermanbergwerf@gmail.com>
2015-06-24 22:47:37 +0200

    Fix CIF mirror CORS for embed.molview.org

--- php/cif.php
+++ php/cif.php
@@ -30,47 +30,53 @@ error_reporting(0);
 parse_str($_SERVER["QUERY_STRING"]);
 header("Content-Type: text");
 
+//allow embed.molview.org
+if($_SERVER["HTTP_ORIGIN"] == "http://embed.molview.org")
+{
+	header("Access-Control-Allow-Origin: ".$_SERVER["HTTP_ORIGIN"]);
+}
+
 //connect to cod
 $cod = new mysqli("www.crystallography.net", "cod_reader", "", "cod");
 if($cod -> connect_errno > 0)
 {
-    http_response_code(500);
-    echo "Internal Error";
-    return;
+	http_response_code(500);
+	echo "Internal Error";
+	return;
 }
 
 //lookup record in database
 $query = 'SELECT flags FROM data WHERE file="'.$codid.'"';
 if($row = $cod -> query($query) -> fetch_row())
 {
-    $cod -> close();
-    if(strpos($row[0], "has coordinates") !== false)
-    {
-        //get cif
-        $cif = get_curl("http://www.crystallography.net/".$codid.".cif");
-        if($cif === false)
-        {
-            http_response_code(404);
-            echo "Not Found";
-            return;
-        }
-        else
-        {
-            echo $cif;
-            return;
-        }
-    }
-    else
-    {
-        http_response_code(404);
-        echo "Not Found";
-        return;
-    }
+	$cod -> close();
+	if(strpos($row[0], "has coordinates") !== false)
+	{
+		//get cif
+		$cif = get_curl("http://www.crystallography.net/".$codid.".cif");
+		if($cif === false)
+		{
+			http_response_code(404);
+			echo "Not Found";
+			return;
+		}
+		else
+		{
+			echo $cif;
+			return;
+		}
+	}
+	else
+	{
+		http_response_code(404);
+		echo "Not Found";
+		return;
+	}
 }
 else
 {
-    $cod -> close();
-    http_response_code(404);
-    echo "Not Found";
-    return;
+	$cod -> close();
+	http_response_code(404);
+	echo "Not Found";
+	return;
 }
