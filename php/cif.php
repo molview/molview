fa0c75dc172f788c4ad564890de07daeb45cc4b2
Herman Bergwerf <post@hbergwerf.nl>
2025-05-19 10:10:06 +0200

    Refactor cif.php with PDO.

--- php/cif.php
+++ php/cif.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of MolView (http://molview.org)
  * Copyright (c) 2014-2023 Herman Bergwerf
@@ -11,61 +12,53 @@ PHP script for mirroring CIF files form the Crystallography Open Database
 @return CIF file or HTTP error
 */
 
-include_once("utility.php");
+include_once('utility.php');
 
-parse_str($_SERVER["QUERY_STRING"], $params);
-$codid = $params["codid"] ?? "";
+parse_str($_SERVER['QUERY_STRING'], $params);
+$codid = intval($params['codid'] ?? '');
 
-header("Content-Type: text");
+header('Content-Type: text');
 
-//allow embed.molview.org and molview.org
-$origin = $_SERVER["HTTP_ORIGIN"];
-if($origin == 'http://molview.org' || $origin == 'https://embed.molview.org')
-{
-	header("Access-Control-Allow-Origin: ".$origin);
+// Allow embed.molview.org and molview.org.
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if ($origin == 'http://molview.org' || $origin == 'https://embed.molview.org') {
+  header("Access-Control-Allow-Origin: .$origin");
 }
 
-//connect to cod
-$cod = new mysqli("www.crystallography.net", "cod_reader", "", "cod");
-if($cod -> connect_errno > 0)
-{
-	http_response_code(500);
-	echo "Internal Error";
-	return;
+// Connect to COD MySQL database using PDO.
+try {
+  $dsn = 'mysql:host=www.crystallography.net;dbname=cod';
+  $db = new PDO($dsn, 'cod_reader', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ]);
+} catch (PDOException $e) {
+  http_response_code(500);
+  echo 'Failed to open COD database.';
+  return;
 }
 
-//lookup record in database
-$query = 'SELECT flags FROM data WHERE file="'.$codid.'"';
-if($row = $cod -> query($query) -> fetch_row())
-{
-	$cod -> close();
-	if(strpos($row[0], "has coordinates") !== false)
-	{
-		//get cif
-		$cif = http_get("http://www.crystallography.net/".$codid.".cif");
-		if($cif === false)
-		{
-			http_response_code(404);
-			echo "Not Found";
-			return;
-		}
-		else
-		{
-			echo $cif;
-			return;
-		}
-	}
-	else
-	{
-		http_response_code(404);
-		echo "Not Found";
-		return;
-	}
-}
-else
-{
-	$cod -> close();
-	http_response_code(404);
-	echo "Not Found";
-	return;
+// Lookup record in database.
+$s = $db->prepare('SELECT flags FROM data WHERE file = :codid');
+$s->execute(['codid' => $codid]);
+if ($row = $s->fetch(PDO::FETCH_ASSOC)) {
+  if (strpos($row['flags'], 'has coordinates') !== false) {
+    // Download CIF file.
+    $cif = http_get("http://www.crystallography.net/$codid.cif");
+    if ($cif === false) {
+      http_response_code(404);
+      echo 'Failed to download CIF file.';
+      return;
+    } else {
+      echo $cif;
+      return;
+    }
+  } else {
+    http_response_code(404);
+    echo 'Record has no coordinates.';
+    return;
+  }
+} else {
+  http_response_code(404);
+  echo 'Record not found.';
+  return;
 }
